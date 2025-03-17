35003d603dd88060284d91b719442fee6017a523
william-darma-wijaya <94919753+william-darma-wijaya@users.noreply.github.com>
2025-03-17 14:15:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,9 @@ def main():
   st.title('Dermatology Machine Learning')
   st.info("This app use machine learning")
 
+  # input data by user
+  erythema = st.slider("Erythema", min_value=0, max_value=2)
+  
+
 if __name__ == "__main__":
   main()
