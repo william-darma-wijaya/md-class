2b820e02fba12a177d36a159e9d5a5871ca5322a
william-darma-wijaya <94919753+william-darma-wijaya@users.noreply.github.com>
2025-03-17 14:44:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,6 +82,8 @@ def main():
 
   age = st.slider('age', min_value = 0, max_value = 75, value = 40)
 
+  gender = st.selectbox("gender", ("male", "female"))
+
   # Input data for program
   user_input = [erythema, scaling, definite_borders, itching, koebner_phenomenon, polygonal_papules, follicular_papules, oral_mucosal_involvement, knee_and_elbow_involvement, scalp_involvement, family_history, melanin_incontinence, eosinophils_infiltrate, PNL_infiltrate, fibrosis_papillary_dermis, exocytosis, acanthosis, hyperkeratosis, parakeratosis, clubbing_rete_ridges, elongation_rete_ridges, thinning_suprapapillary_epidermis, spongiform_pustule, munro_microabcess, focal_hypergranulosis, disappearance_granular_layer, vacuolisation_damage_basal_layer, spongiosis, saw_tooth_appearance_retes, follicular_horn_plug, perifollicular_parakeratosis, inflammatory_mononuclear_infiltrate, band_like_infiltrate, age]
   model_filename = "trained_model.pkl"
