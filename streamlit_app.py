a576182a616d9ac9b5e26107e51293484057adba
william-darma-wijaya <94919753+william-darma-wijaya@users.noreply.github.com>
2025-03-17 14:24:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import joblib
 
+def load_model(filename):
+  model = joblib.load(filename)
+  return model
+
+def predict_with_model(model, user_input):
+  prediction = model.predict([user_input])
+  return prediction[0]
+
 def main():
   st.title('Dermatology Machine Learning')
   st.info("This app use machine learning")
